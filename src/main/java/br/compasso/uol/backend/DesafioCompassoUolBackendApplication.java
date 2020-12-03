2f2701cc983fd3562723ef85539b609684304349
rafaelbrier <rafael.sales@ntendencia.com.br>
2020-12-03 20:14:23 -0300

    Inicialização do Projeto, adição de dependências básicas

---
+++ src/main/java/br/compasso/uol/backend/DesafioCompassoUolBackendApplication.java
@@ -0,0 +1,13 @@
+package br.compasso.uol.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCompassoUolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCompassoUolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/compasso/uol/backend/DesafioCompassoUolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.compasso.uol.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCompassoUolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
